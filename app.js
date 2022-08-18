204c5a7797a0410017d243e46571a1b7978f7a90
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-08-18 10:14:31 -0700

    promises1

--- app.js
+++ app.js
@@ -251,4 +251,20 @@ async function firstSixIncomplete(userId){
 }
 firstSixIncomplete(6);
 
+/*
+
+PROMISES
+
+*/
+
+// fetch('https://jsonplaceholder.typicode.com/users/1');
+const emailRef = document.querySelector(".email");
+
+
+fetch('https://jsonplaceholder.typicode.com/users/1').then((response) => {
+    response.json().then(data => {
+        console.log(data);
+    });
+})
+
 
